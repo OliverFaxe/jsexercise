743d14267ea46c1262bdde491efda75a332b4fdb
OliverFaxe <oliverfaxe@hotmail.com>
2024-10-31 22:16:07 +0100

    klar nu

---
+++ index.js
@@ -0,0 +1,152 @@
+// // ## 1 ##
+// let name = prompt("Hello! What's your name?");
+
+// console.log("Welcome",name);
+
+// // ## 2 ##
+// let word1 = prompt("Choose a word");
+
+// let word2 = prompt("Choose another word");
+
+// if (word1.toLowerCase(0) < word2.toLowerCase(0)) {
+//     console.log(word1);
+// } else {
+//     console.log(word2);
+// }
+// ## 3 ##
+// let age = prompt("Please enter your Age");
+
+// if (age >= 18) {
+//     console.log("You can continue using this service");
+// } else {
+//     console.log("You cannot use these services");
+// }
+// ## 4 ##
+// let answer = "";
+
+// while (answer.toLowerCase() !== "cat") {
+//     answer = prompt("Which animal am I thinking of?");
+
+//     if (answer.toLowerCase() === "dog") {
+//         alert("The other hairy friend!");
+//     } else if (answer.toLowerCase() === "lion") {
+//         alert("Close, but think smaller.");
+//     } else if (answer.toLowerCase() === "cat") {
+//         alert("Yes! Cat!");
+//     } else if (answer.toLowerCase() === "bird") {
+//         alert("What can hunt birds? With no wings.");
+//     } else {
+//         alert("Keep guessing!");
+//     }
+// }
+// ## 5 ##
+
+// let color = prompt("Choose between: red, green, blue, purple - to reveal a secret");
+
+// if (color.toLowerCase() === "red") {
+//     alert("you are action oriented with a deep need for physical fulfillment and to experience life through the five senses.");
+// } else if (color.toLowerCase() === "green") {
+//     alert("you have a deep need to belong, to love and be loved, and to feel safe and secure.");
+// } else if (color.toLowerCase() === "blue") {
+//     alert("deep need to find inner peace and truth, to live their life according to their ideals and beliefs");
+// } else if (color.toLowerCase() === "purple") {
+//     alert("you have a deep need for emotional security and to create order and perfection in all areas of your life, including your spiritual life.");
+// } else {
+//     alert("Choose between RGBP")
+// }
+// ## 6 ##
+
+// confirm("Selfcheck your test press OK to continue");
+
+// let points = Number(prompt("What was the result of your test?"));
+
+// if (points >= 100) {
+//     alert("Aced! - VG");
+// } else if (points >= 50) {
+//     alert("You completed the test - G");
+// } else if (points < 50 && points >= 0) {
+//     alert("You failed the test - F");
+// } else {
+//     alert("Not a valid score");
+// }
+// ## 7 ##
+
+// alert("What type of weather is it today?");
+
+// let answer = prompt("warm, rainy, cool, cold");
+
+// if (answer.toLowerCase() == "warm") {
+//   alert("Go out and enjoy the sun!");
+// } else if (answer.toLowerCase() === "rainy") {
+//   alert("Stay inside and play games or watch a movie!");
+// } else if (answer.toLowerCase() === "cool") {
+//   alert("Go exercise and explore the nature!");
+// } else if (answer.toLowerCase() === "cold") {
+//   alert("Cozy up and read a book");
+// } else {
+//   alert("Go pet your cat");
+// }
+
+// ## 8 ##
+
+// let language = prompt("What language do you speak?");
+
+// if (language.toLowerCase() === "swedish") {
+//   alert("Hallå snaphane");
+// } else if (language.toLowerCase() === "english") {
+//   alert("Go learn some Swedish");
+// } else if (language.toLowerCase() === "japanese") {
+//   alert("Konnichiwa kawai");
+// } else {
+//   alert("You should really try learning Swedish");
+// }
+
+// ## 9 ##
+
+// let tal = prompt("Skriv ett tal")
+
+// if (tal % 5 === 0){
+//     alert(`${tal} is a multiple of 5`)
+// } else {
+//     alert(`${tal} is not a multiple of 5`)
+// }
+
+// ## 10 ##
+
+// let userWantsToGoOut = confirm("Do you want to go out?");
+
+// if (userWantsToGoOut) {
+//   alert("How fun!");
+// } else {
+//   alert("Tell me if you change your mind!");
+// }
+
+//## 11 ##
+
+// let userInput = prompt("Enter a character");
+
+// if (!isNaN(userInput)) {
+//   alert("Its a number!");
+// } else {
+//   alert("Its a string of characters");
+// }
+
+//## 12 ##
+
+// let word1 = prompt("Type in one word");
+
+// let word2 = prompt("Type in another");
+
+// let word3 = prompt("One last!");
+
+// if (word1.length > word2.length && word1.length > word3.length){
+//     alert(`${word1} is the longest word`)
+// } else if ( word2.length > word1.length && word2.length > word3.length){
+//     (alert(`${word2} is the longest word`))
+// } else {
+//     (alert(`${word3} is the longest word`))
+// }
+
+//## 13 ##
+
+
